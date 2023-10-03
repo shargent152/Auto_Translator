80d9ecb87839b69a232866c8b514db7f0d681d16
Trevor Peevers <shargent152@gmail.com>
2023-10-03 00:28:49 -0400

    saving

--- app/src/main/java/com/example/auto_translator/MainActivity.kt
+++ app/src/main/java/com/example/auto_translator/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.auto_translator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
+var sourceLang = 0
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null){
+            var fragment = supportFragmentManager.beginTransaction().add(R.id.frag,edittext.newInstance(),"editText").commit()
+                fragment
+        }
+
+    }
+
+    fun english_translate(view: View) {
+        if(sourceLang == 1){
+            // does nothing same language
+
+        }
+        else if (sourceLang == 2){
+            // goes to spanish
+        }
+        else if (sourceLang == 3){
+            // goes to german
+        }
+    }
+    fun spanish_translate(view: View) {}
+    fun german_translate(view: View) {}
+    fun english_sourceLanguage(view: View) {
+        sourceLang = 1
+    }
+    fun spanish_sourceLanguage(view: View) {
+        sourceLang = 2
+    }
+    fun german_sourceLanguage(view: View) {
+        sourceLang = 3
     }
 }
+
+
---
+++ app/src/main/java/com/example/auto_translator/edittext.kt
@@ -0,0 +1,57 @@
+package com.example.auto_translator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [edittext.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class edittext : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edittext,container)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment edittext.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+            edittext().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
